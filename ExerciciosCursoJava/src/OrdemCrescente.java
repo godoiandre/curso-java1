42ec91174433814cd1af48a9d87c775388d241b0
GodoiAndre <andregodoij@gmail.com>
2025-02-06 15:26:45 -0300

    Adionando o repositorio curso-java

---
+++ ExerciciosCursoJava/src/OrdemCrescente.java
@@ -0,0 +1,50 @@
+/*
+ * Crie um algoritmo que leia 3 numeors e mostre na tela em ordem crescente
+ */
+
+import java.util.Scanner;
+
+public class OrdemCrescente {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite 3 numeros:");
+        //Lendo os numeros
+        double n1 = scanner.nextDouble();
+        double n2 = scanner.nextDouble();
+        double n3 = scanner.nextDouble();
+
+        //Colocando-os em ordem crescente
+        if(n1<n2 && n1<n3){
+            System.out.println(n1);
+            if(n2<n3){
+                System.out.println(n2);
+                System.out.println(n3);
+            } else{
+                System.out.println(n3);
+                System.out.println(n2);
+            }
+
+        } else if(n2<n1 && n2<n3){
+            System.out.println(n2);
+            if(n1<n3){
+                System.out.println(n1);
+                System.out.println(n3);
+            } else{
+                System.out.println(n3);
+                System.out.println(n1);
+            }
+        } else{
+            System.out.println(n3);
+            if(n1<n2){
+                System.out.println(n1);
+                System.out.println(n2);
+            } else{
+                System.out.println(n2);
+                System.out.println(n1);
+            }
+        }
+        scanner.close();
+
+    }
+    
+}
